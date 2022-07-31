acb3676c03741304ec1d37b1b8ea8218a9ec1a5b
mikishoma <19267@toba-cmt.ac.jp>
2022-07-31 19:18:45 +0900

    リセット実装

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
  //ランダムの文字
  // 生成する文字列の長さ
  var l = 1;
-
  // 生成する文字列に含める文字セット
  var c = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわ";
 
@@ -30,8 +29,29 @@ serve(async req => {
     if (req.method === "POST" && pathname === "/shiritori") {
         const requestJson = await req.json();
         const nextWord = requestJson.nextWord;
+        //リセットを押したら
+        if(nextWord==0){
 
-        //「ん
+          //ランダムの文字
+        // 生成する文字列の長さ
+        var l = 1;
+        // 生成する文字列に含める文字セット
+        var c = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわ";
+
+        var cl = c.length;
+        var r = "";
+        for(var i=0; i<l; i++){
+          r += c[Math.floor(Math.random()*cl)];
+        }
+
+        word.length = 0;
+          // c=word.length;
+          previousWord =r;
+          return new Response(previousWord);
+        }
+
+
+        //「ん」で終わったとき
         if(nextWord.charAt(nextWord.length -1)=="ん"){
           return new Response("「ん」で終わりました。",{ status: 400 });
         }
@@ -65,9 +85,7 @@ serve(async req => {
         }
         }
     
-
         word.push(nextWord);
-
         previousWord = nextWord;
         return new Response(previousWord);
         }
@@ -79,5 +97,9 @@ serve(async req => {
         enableCors: true,
       });
 
+   
 });
+
+
+
 
